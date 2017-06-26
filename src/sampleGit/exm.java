34fe0a58f8995556b43095a47e8c036eb1d79896
sy_an <sy_an@EP4440WKP.p-eplus.local>
2017-06-26 12:07:22 +0900

    class

--- src/sampleGit/exm.java
+++ src/sampleGit/exm.java
@@ -5,13 +5,17 @@ public class exm {
 	public static void main(String[] args) {
 		System.out.print("hello world!");
 		
-		String [] hello = {"1","2","3","4","5"};
+		String [] hello = {"1","2","3",null,"5"};
 		
 		for(String hi : hello){
 			System.out.println(hi);
 			
 		}
 		System.out.println("master");
+		
+		newPrint np = new newPrint();
+		boolean result = np.isExt(hello);
+		System.out.println(Boolean.toString(result));
 
 	}
 
