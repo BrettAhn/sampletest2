f6acfaaa4eaa88e7ffbd6a74d462aaf3711c93c9
sy_an <sy_an@EP4440WKP.p-eplus.local>
2017-06-26 11:14:13 +0900

    exp For

--- src/sampleGit/exm.java
+++ src/sampleGit/exm.java
@@ -4,6 +4,12 @@ public class exm {
 
 	public static void main(String[] args) {
 		System.out.print("hello world!");
+		
+		String [] hello = {"1","2","3","4","5"};
+		
+		for(String hi : hello){
+			System.out.println(hi);
+		}
 
 	}
 
